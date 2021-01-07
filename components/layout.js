dfec57bda8665b40c471e7c2e414d74d07905a38
Edmundo Paulino <manyymoore@gmail.com>
2021-01-07 15:59:51 -0300

    Updates

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Edmundo Paulino';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Edmundo\u2019s Blog';
 
 export default function Layout({ children, home }) {
   return (
